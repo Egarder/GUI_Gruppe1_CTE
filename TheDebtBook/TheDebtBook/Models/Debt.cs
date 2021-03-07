c037a02e89b8ac4847f6bddbe8fc80214f3d9143
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-03-07 14:29:39 +0100

    - added debts

---
+++ TheDebtBook/TheDebtBook/Models/Debt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace TheDebtBook.Models
+{
+    class Debt
+    {
+        private double _amount;
+
+        // Default constructor
+        public Debt()
+        { }
+
+        // Explicit constructor
+        public Debt(double amount)
+        {
+            _amount = amount;
+        }
+
+        public double Amount
+        {
+            get => _amount;
+            set => _amount = value;
+        }
+    }
+}
